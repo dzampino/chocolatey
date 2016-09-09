27fcf3251c6341088f019cfdf8832d6986053654
David Zampino <dzampino@users.noreply.github.com>
2016-09-09 12:35:29 -0500

    Added altools

---
+++ altools/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageArgs = @{
+  packageName    = 'altools'
+  fileType       = 'exe'
+  silentArgs     = "/Q /T:$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  url            = 'https://download.microsoft.com/download/1/f/0/1f0e9569-3350-4329-b443-822976f29284/ALTools.exe'
+  validExitCodes = @(0,3010)
+  checksum       = '716718c095ef5ad5724bc8344cbccc176e14fd773de89c517f6c8514cd3ae260'
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
